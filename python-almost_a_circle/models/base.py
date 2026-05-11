2682104d9f3be1c48c478c284e95b4236704e440
Sənin Adın <snin@email.com>
2026-05-11 11:59:52 +0000

    Add from_json_string to base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -32,3 +32,10 @@ class Base:
         list_dicts = [obj.to_dictionary() for obj in list_objs]
         with open(filename, "w") as f:
             f.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list represented by json_string"""
+        if not json_string:
+            return []
+        return json.loads(json_string)

c32905d02519af896dc37c4730f7927e93dd34c9
Sənin Adın <snin@email.com>
2026-05-11 11:53:36 +0000

    Add update method to square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,6 +21,17 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Assigns arguments to each attribute"""
+        attrs = ['id', 'size', 'x', 'y']
+        if args:
+            for i, value in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
     def __str__(self):
         """Returns string representation of Square"""
         return "[Square] ({}) {}/{} - {}".format(
